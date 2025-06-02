1393714a8ae0f3d9907fcbdd183f81f33284a5c7
W.A.Punsisi Yemani Perera <wapunsisiyemani18@gmail.com>
2025-06-02 23:29:11 +0530

    JWT key hiding🔑

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,14 @@
 import jwt from 'jsonwebtoken';
+import dotenv from'dotenv';
+//load dotenv
+dotenv.config()
 
 export default function verifyJWT(req,res,next){
         const header = req.header("Authorization");      
         if(header != null){
             const token = header.replace("Bearer ","");
             //console.log(token);
-            jwt.verify(token,"random456",
+            jwt.verify(token,process.env.JWT_KEY,
                 (err,decoded)=>{
                     console.log(decoded);
 
